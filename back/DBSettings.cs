d86be58d95234a690c1459604f04e8062dc77f84
Bernardo de Sousa Garcia <bernardo.sousa.garcia@gmail.com>
2024-05-17 21:23:56 -0300

    Configuração Backend

---
+++ back/DBSettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using project_renault.Entities;
+
+namespace project_renault
+{
+    public class DBSettings : DbContext
+    {
+        private IConfiguration _configuration;
+
+        public DBSettings(IConfiguration configuration, DbContextOptions options) { 
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("MySql");
+            optionsBuilder.UseMySQL(connectionString);
+        }
+    }
+}
