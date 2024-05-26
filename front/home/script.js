cb4a146c4456c8940c2359c913011fec82d0e07c
Matheus Guimarães <82780534+HolaSoyMath@users.noreply.github.com>
2024-05-26 20:05:32 -0300

    V0.1
    -- Legenda gráfico para apenas números inteiros
    -- Largura do gráfico ajustada

--- front/home/script.js
+++ front/home/script.js
@@ -1,85 +1,3 @@
-function substituirNomeUsuario() {
-  // Coletar o nome do usuário salvo no Session Storage
-  var nome = coletarNomeUsuario();
-
-  // Selecionar todos os elementos com a classe 'nome'
-  var elementos = document.querySelectorAll(".nome");
-
-  // Substituir o texto dentro de cada elemento pelo nome
-  elementos.forEach(function(elemento) {
-      elemento.textContent = nome;
-  });
-}
-
-// Evento para rodar a função quando a página carregar
-window.addEventListener('DOMContentLoaded', substituirNomeUsuario);
-
-function inserirGrafico(){
-	// Puxar os dados necessários
-	const dados = coletarDados();
-
-	// Criar um Object com os nomes dos projetos
-	var nomeInfo = gerarLabel(dados);
-
-	// Criar um Array com quantas vezes os projetos aparecem
-	var valorInfo = gerarNúmeros(dados, nomeInfo);
-	
-	// Transformar Object em Array
-	nomeInfo = Array.from(nomeInfo);
-
-	// Criar o Gráfico
-	const ctx = document.getElementById('myChart');
-	new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: nomeInfo,
-			datasets: [{
-				label: 'Número de riscos',
-				data: valorInfo,
-				borderWidth: 0
-			}]
-		},
-		options: {
-			scales: {
-				x: {
-					grid: {
-						display: false,
-					}
-				},
-				y: {
-					beginAtZero: true,
-					grid: {
-						display: false,
-					},
-				}
-			}
-		}
-	});
-}
-
-// Evento para rodar a função quando a página carregar
-window.addEventListener('DOMContentLoaded', inserirGrafico);
-
-function gerarLabel(riscos){
-	// Coletar apenas os nomes únicos
-	const projetosUnicosObj = new Set(riscos.map(risco => risco.projeto));
-	const projetosUnicosArr = Array.from(projetosUnicosObj);
-	return projetosUnicosArr;
-}
-
-function gerarNúmeros(riscos, infoUnica){
-	// criar um array para os retornos
-	const contagens = [];
-
-	// Coletar apenas os nomes únicos
-	const nomesProjetos = riscos.map(risco => risco.projeto);
-	infoUnica.forEach((projeto) => {
-		var contagem = nomesProjetos.filter(projetoVerificado => projetoVerificado === projeto).length;
-		contagens.push(contagem);
-	})
-	return contagens;
-}
-
 // fetch('http://localhost:8080/risk/getAll')
 //   .then(response => response.json())
 //   .then(data => {
@@ -170,11 +88,191 @@ function coletarDados(){
 			"jalonAfetado": "Jalon 2",
 			"metier": "Metier 2",
 			"status": 2
+		},
+		{
+			"id": 1,
+			"descricaoRisco": "Descrição do risco 1",
+			"tipo": "Tipo 1",
+			"probabilidade": "Alta",
+			"areaResponsavel": "Área 1",
+			"classificacaoRisco": "Classe 1",
+			"projeto": "Projeto 2",
+			"dataEntradaRisco": "2024-05-22T00:00:00",
+			"impacto": "Alto",
+			"impactoRenault": "Alto",
+			"consequencias": "Consequências 1",
+			"jalonAfetado": "Jalon 1",
+			"metier": "Metier 1",
+			"status": 1
+		},
+		{
+			"id": 2,
+			"descricaoRisco": "Descrição do risco 2",
+			"tipo": "Tipo 2",
+			"probabilidade": "Média",
+			"areaResponsavel": "Área 2",
+			"classificacaoRisco": "Classe 2",
+			"projeto": "Projeto 2",
+			"dataEntradaRisco": "2024-05-23T00:00:00",
+			"impacto": "Médio",
+			"impactoRenault": "Médio",
+			"consequencias": "Consequências 2",
+			"jalonAfetado": "Jalon 2",
+			"metier": "Metier 2",
+			"status": 2
+		},
+		{
+			"id": 1,
+			"descricaoRisco": "Descrição do risco 1",
+			"tipo": "Tipo 1",
+			"probabilidade": "Alta",
+			"areaResponsavel": "Área 1",
+			"classificacaoRisco": "Classe 1",
+			"projeto": "Projeto 2",
+			"dataEntradaRisco": "2024-05-22T00:00:00",
+			"impacto": "Alto",
+			"impactoRenault": "Alto",
+			"consequencias": "Consequências 1",
+			"jalonAfetado": "Jalon 1",
+			"metier": "Metier 1",
+			"status": 1
+		},
+		{
+			"id": 2,
+			"descricaoRisco": "Descrição do risco 2",
+			"tipo": "Tipo 2",
+			"probabilidade": "Média",
+			"areaResponsavel": "Área 2",
+			"classificacaoRisco": "Classe 2",
+			"projeto": "Projeto 4",
+			"dataEntradaRisco": "2024-05-23T00:00:00",
+			"impacto": "Médio",
+			"impactoRenault": "Médio",
+			"consequencias": "Consequências 2",
+			"jalonAfetado": "Jalon 2",
+			"metier": "Metier 2",
+			"status": 2
+		},
+		{
+			"id": 2,
+			"descricaoRisco": "Descrição do risco 2",
+			"tipo": "Tipo 2",
+			"probabilidade": "Média",
+			"areaResponsavel": "Área 2",
+			"classificacaoRisco": "Classe 2",
+			"projeto": "Projeto 5",
+			"dataEntradaRisco": "2024-05-23T00:00:00",
+			"impacto": "Médio",
+			"impactoRenault": "Médio",
+			"consequencias": "Consequências 2",
+			"jalonAfetado": "Jalon 2",
+			"metier": "Metier 2",
+			"status": 2
 		}
 	]
 
 	return riscos;
 }
+
+function substituirNomeUsuario() {
+  // Coletar o nome do usuário salvo no Session Storage
+  var nome = coletarNomeUsuario();
+
+  // Selecionar todos os elementos com a classe 'nome'
+  var elementos = document.querySelectorAll(".nome");
+
+  // Substituir o texto dentro de cada elemento pelo nome
+  elementos.forEach(function(elemento) {
+      elemento.textContent = nome;
+  });
+}
+
+// Evento para rodar a função quando a página carregar
+window.addEventListener('DOMContentLoaded', substituirNomeUsuario);
+
+function inserirGrafico(){
+	// Puxar os dados necessários
+	const dados = coletarDados();
+
+	// Criar um Object com os nomes dos projetos
+	var nomeInfo = gerarLabel(dados);
+
+	// Criar um Array com quantas vezes os projetos aparecem
+	var valorInfo = gerarNúmeros(dados, nomeInfo);
+	
+	// Transformar Object em Array
+	nomeInfo = Array.from(nomeInfo);
+
+	// Criar o Gráfico
+	const ctx = document.getElementById('myChart');
+	new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: nomeInfo,
+			datasets: [{
+				label: 'Número de riscos',
+				data: valorInfo,
+				borderWidth: 0
+			}]
+		},
+		options: {
+			scales: {
+				x: {
+					grid: {
+						display: false,
+					}
+				},
+				y: {
+					beginAtZero: true,
+					grid: {
+						display: false,
+					},
+					ticks: {
+						stepSize: 1,
+					}
+				},
+			}
+		}
+	});
+}
+
+// Evento para rodar a função quando a página carregar
+window.addEventListener('DOMContentLoaded', inserirGrafico);
+
+function gerarLabel(riscos){
+	// Coletar apenas os nomes únicos
+	const projetosUnicosObj = new Set(riscos.map(risco => risco.projeto));
+	const projetosUnicosArr = Array.from(projetosUnicosObj);
+	return projetosUnicosArr;
+}
+
+function gerarNúmeros(riscos, infoUnica){
+	// criar um array para os retornos
+	const contagens = [];
+
+	// Coletar apenas os nomes únicos
+	const nomesProjetos = riscos.map(risco => risco.projeto);
+	infoUnica.forEach((projeto) => {
+		var contagem = nomesProjetos.filter(projetoVerificado => projetoVerificado === projeto).length;
+		contagens.push(contagem);
+	})
+	return contagens;
+}
+
+function alterarLarguraContainerGrafico() {
+	// Setar container grafico
+	const contGrafico = document.getElementById("grafico");
+
+	// Coletar tamanho do container dos cards
+	const larguraContCard = document.getElementById("containerCard").offsetWidth;
+
+	// Alterar o tamanho da largura
+	contGrafico.style.width = larguraContCard + "px";
+
+}
+
+// Evento para rodar a função quando a página carregar
+window.addEventListener('DOMContentLoaded', alterarLarguraContainerGrafico);
 
 // Incluir riscos na tabela
 function popularTabela(){
