077dae01ff25e983b1bd96814cfa0c3a0347bde2
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-06-03 00:22:36 -0300

    IA com provavel overfitting

--- IA/ia.py
+++ IA/ia.py
@@ -2,8 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+from joblib import dump
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from warnings import simplefilter
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
 
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
@@ -17,83 +22,82 @@ else:
 try:
     df = pd.read_excel(file_path, header=4, usecols="C:AF", sheet_name="List of risks")
     
+    df.columns = df.columns.str.strip()
+
     df.drop(
         ['Risco', 'Tipo de risco', 'Data de entrada do risco', 'Metier', 'Ação', 'Nome do Piloto', "ID do Piloto", "Início do Plano de Ação", "Data de Alerta", "Tempo de Resolução", "Hora", "Comentários", "Data de Resolução", "Status", "Capitalização"],
-        axis = 1,
-        inplace = True
+        axis=1,
+        inplace=True
+    )
+    df.drop(
+        ['Probabilidade' ,'Impacto' ,'Impacto/Consequências', 'Jalón Impactado no Futuro', 'Estratégia', 'Probabilidade Residual', 'Impacto Residual', 'Classificação de Risco Residual', 'Validação da Ação', 'Validação do Risco'],
+        axis=1,
+        inplace=True
     )
 
-    df.dropna()
+    df.dropna(inplace=True)
 
     print("Nomes das colunas:")
     print(df.columns)
 
-    ##########################
+    Y = df['Classificação de Risco']
+    X = df.drop('Classificação de Risco', axis=1)
 
-    #########################
+    categorical_columns = X.select_dtypes(include=['object']).columns
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', OneHotEncoder(), categorical_columns)
+        ],
+        remainder='passthrough'
+    )
+
+    X_transformed = preprocessor.fit_transform(X)
+
+    # onehot_columns = preprocessor.named_transformers_['cat'].get_feature_names_out(categorical_columns)
+
+    # all_columns = list(onehot_columns) + list(X.select_dtypes(exclude=['object']).columns)
+
+    # X_transformed_df = pd.DataFrame(X_transformed.toarray(), columns=all_columns)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X_transformed, Y, test_size=0.30, random_state=42)
+
+    model = RandomForestClassifier()
+    model.fit(X_train, Y_train)
+
+    dump(model, 'model.pkl')
+
+    train_predictions = model.predict(X_train)
+    test_predictions = model.predict(X_test)
+
+    print(f"Precisão no conjunto de treinamento: {precision_score(Y_train, train_predictions, average='weighted'):.2f}")
+    print(f"Precisão no conjunto de teste: {precision_score(Y_test, test_predictions, average='weighted'):.2f}")
+
+    Ypred = model.predict(X_transformed)
+    plt.plot(Y.reset_index(drop=True), label='True Labels')
+    plt.plot(Ypred, label='Predicted Labels')
+    plt.legend()
+    plt.show()
+
+    # Printando valores codificados pelo OnehotEncoder
+
+    # print("Dados transformados:")
+    # print(X_transformed_df.head())
+
+    # unique_values = {}
+    # for col in categorical_columns:
+    #     unique_values[col] = X[col].unique()
+
+    # class_labels = {}
+    # for col, values in unique_values.items():
+    #     for i, value in enumerate(values):
+    #         class_labels[f"{col}_{i}"] = value
+
+    # Imprimir as classes de risco
+    # print("Classes de risco:")
+    # print(class_labels)
 
 except FileNotFoundError:
     print(f"O arquivo {file_path} não foi encontrado. Verifique o caminho e tente novamente.")
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
-
-
-# df.drop(
-#     ['id', 'created_at', 'white_id', 'black_id', 'increment_code', 'opening_eco', "last_move_at", "rated", "opening_name", "black_rating", "opening_ply"],
-#     axis = 1,
-#     inplace = True
-# )
-
-# le = LabelEncoder()
-
-# print("Comecou!!")
-
-# df['moves'] = df["moves"].str.split()
-# moves = df['moves']
-# i = 0
-
-# for sublist in moves:
-#     moves = sublist[0: 10: 2]
-#     for item in sublist:
-#         if item in df.loc[i, 'moves']:
-#             df.loc[i, item] = 1
-#     i += 2
-#     if i > 20056:
-#         print("Terminou!!")
-#         break
-# df = df.fillna((int)(0))
-
-# df.drop(
-#     [ 'moves'],
-#     axis = 1,
-#     inplace = True
-# )
-
-# df['victory_status'] = le.fit_transform(df['victory_status'])
-# df['winner'] = le.fit_transform(df['winner'])
-# print(le.classes_)
-
-# Y = df['winner']
-# X = df
-
-# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
-
-# model = MiniBatchKMeans(n_clusters=6,
-#                         random_state=42,
-#                         batch_size=20,
-#                         max_iter=500,
-#                         n_init="auto")
-# model.fit(X_train, X_test)
-# dump(model, 'chess.pkl')
-
-# preds = model.predict(X)
-# df['cluster'] = preds
-
-# data = list(zip(preds, Y, df['d4'].values.tolist()))
-
-
-# plt.figure(figsize=(10, 8))
-# sns.set_theme(style="ticks")
-# sns.pairplot(df.sample(1000), vars=df.columns[0:8], hue='cluster', palette='viridis')
-# plt.suptitle('Matriz de Dispersão (Scatter Matrix) para as primeiras 5 variáveis', y=1.02)
-# plt.show()
