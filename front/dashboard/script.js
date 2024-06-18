52231755a811c98d552ce3c9468e14a66e3c4bf1
Matheus Guimarães <82780534+HolaSoyMath@users.noreply.github.com>
2024-06-18 20:51:06 -0300

    V0.5
    - Tela de Dashboard feita

--- front/dashboard/script.js
+++ front/dashboard/script.js
@@ -13,3 +13,49 @@ function substituirNomeUsuario() {
 
 // Evento para rodar a função quando a página carregar
 window.addEventListener('DOMContentLoaded', substituirNomeUsuario);
+
+function gerarGrafico(grafico){
+  new Chart(grafico, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+				x: {
+					grid: {
+						display: false,
+					}
+				},
+				y: {
+					beginAtZero: true,
+					grid: {
+						display: false,
+					},
+					ticks: {
+						stepSize: 1,
+					}
+				},
+			}
+    }
+  });
+}
+
+function listagraficos(){
+  let graficos = document.getElementsByTagName('canvas'); // Corrigir o seletor para 'canvas'
+
+  // Converter HTMLCollection para Array para usar forEach
+  Array.from(graficos).forEach(grafico => {
+    gerarGrafico(grafico);
+  });
+}
+
+// Evento para rodar a função quando a página carregar
+window.addEventListener('DOMContentLoaded', listagraficos);
