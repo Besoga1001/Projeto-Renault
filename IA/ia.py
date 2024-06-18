c480e17a7c8b3b6b751eaf94ec72eaaeb2cb3eea
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-06-17 21:35:03 -0300

    input for IA

--- IA/ia.py
+++ IA/ia.py
@@ -25,12 +25,13 @@ try:
     df.columns = df.columns.str.strip()
 
     df.drop(
-        ['Risco', 'Tipo de risco', 'Data de entrada do risco', 'Metier', 'Ação', 'Nome do Piloto', "ID do Piloto", "Início do Plano de Ação", "Data de Alerta", "Tempo de Resolução", "Hora", "Comentários", "Data de Resolução", "Status", "Capitalização"],
-        axis=1,
-        inplace=True
-    )
-    df.drop(
-        ['Probabilidade' ,'Impacto' ,'Impacto/Consequências', 'Jalón Impactado no Futuro', 'Estratégia', 'Probabilidade Residual', 'Impacto Residual', 'Classificação de Risco Residual', 'Validação da Ação', 'Validação do Risco'],
+        [
+            'Risco', 'Tipo de risco', 'Data de entrada do risco', 'Metier', 'Ação', 'Nome do Piloto', "ID do Piloto", 
+            "Início do Plano de Ação", "Data de Alerta", "Tempo de Resolução", "Hora", "Comentários", "Data de Resolução", 
+            "Status", "Capitalização", 'Probabilidade', 'Impacto', 'Impacto/Consequências', 'Jalón Impactado no Futuro', 
+            'Estratégia', 'Probabilidade Residual', 'Impacto Residual', 'Classificação de Risco Residual', 'Validação da Ação', 
+            'Validação do Risco'
+        ],
         axis=1,
         inplace=True
     )
@@ -54,18 +55,13 @@ try:
 
     X_transformed = preprocessor.fit_transform(X)
 
-    # onehot_columns = preprocessor.named_transformers_['cat'].get_feature_names_out(categorical_columns)
-
-    # all_columns = list(onehot_columns) + list(X.select_dtypes(exclude=['object']).columns)
-
-    # X_transformed_df = pd.DataFrame(X_transformed.toarray(), columns=all_columns)
-
     X_train, X_test, Y_train, Y_test = train_test_split(X_transformed, Y, test_size=0.30, random_state=42)
 
     model = RandomForestClassifier()
     model.fit(X_train, Y_train)
 
-    dump(model, 'model.pkl')
+    dump(preprocessor, 'IA/models/preprocessor.pkl')
+    dump(model, 'IA/models/model.pkl')
 
     train_predictions = model.predict(X_train)
     test_predictions = model.predict(X_test)
@@ -78,24 +74,6 @@ try:
     plt.plot(Ypred, label='Predicted Labels')
     plt.legend()
     plt.show()
-
-    # Printando valores codificados pelo OnehotEncoder
-
-    # print("Dados transformados:")
-    # print(X_transformed_df.head())
-
-    # unique_values = {}
-    # for col in categorical_columns:
-    #     unique_values[col] = X[col].unique()
-
-    # class_labels = {}
-    # for col, values in unique_values.items():
-    #     for i, value in enumerate(values):
-    #         class_labels[f"{col}_{i}"] = value
-
-    # Imprimir as classes de risco
-    # print("Classes de risco:")
-    # print(class_labels)
 
 except FileNotFoundError:
     print(f"O arquivo {file_path} não foi encontrado. Verifique o caminho e tente novamente.")
