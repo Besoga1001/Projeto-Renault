33d3a954917424cd239e687f2cbaf31de11479f9
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-06-26 21:14:02 -0300

    IA Structure ready

--- IA/server.py
+++ IA/server.py
@@ -1,33 +1,26 @@
 from flask import Flask, request, jsonify
 import joblib
 
-# Initialize Flask application
 app = Flask(__name__)
 
-# Load the trained machine learning model
 model = joblib.load('model.joblib')
 
-# Define the prediction endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
 
         input_data = request.json
 
-        input1 = input_data['input1']
-        input2 = input_data['input2']
-        input3 = input_data['input3']
-        input4 = input_data['input4']
+        reponsavel = input_data['reponsavel']
+        projeto = input_data['projeto']
+        jalon_Afetado = input_data['jalon_Afetado']
 
-        data = [input1, input2, input3, input4]
+        data = [reponsavel, projeto, jalon_Afetado]
         prediction = model.predict([data])[0]
 
-        # Return the prediction as a JSON response
         return jsonify({'prediction': prediction}), 200
     except Exception as e:
-        # Handle any errors
         return jsonify({'error': str(e)}), 500
 
-# Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
